9a0c544b1adf4df14078b2123fe9c5dc12e9f21b
Joan <joan.wafula@student.moringaschool.com>
2022-08-08 02:34:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    
+    const messages = [];
+   
+     for (let i = 0; i < names.length; i++) {
+   const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+     }
+     return messages;
+   
+   }
+
+ var countDown=function(i) {
+        console.log(i);
+        i>0 && countDown(i-1);
+    }
+        countDown(10);
+
+  
